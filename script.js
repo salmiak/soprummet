08b964274605184c14d3d4328775412fd3bd0fe5
Alfred Beckman <alfred@salmiakmedia.se>
2021-01-12 08:40:39 +0100

    Show day for next pickup, fixed bug when early same day.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let now = moment().set({
+let twoAClock = moment().set({
   'hour': 14,
   'minute': 0,
   'second': 0,
@@ -9,44 +9,44 @@ let dates = [
   {
     // Mondays
     id: 'paper',
-    start: moment(now).day( moment().day() >= 1 ? 1 :-6 ),
-    stop:  moment(now).day( moment().day() >= 1 ? 1 :-6 ).add(1,'w')
+    start: moment(twoAClock).day( moment(twoAClock).day() >= 1 ? 1 :-6 ),
+    stop:  moment(twoAClock).day( moment(twoAClock).day() >= 1 ? 1 :-6 ).add(1,'w')
   },
   {
     // Mondays and Friday
     id: 'well',
-    start: moment(now).day( moment().day() >= 5 ? 5 : moment().day() >= 1 ? 1 : -2 ),
-    stop:  moment(now).day( moment().day() >= 5 ? 8 : moment().day() >= 1 ? 5 : 1 )
+    start: moment(twoAClock).day( moment(twoAClock).day() >= 5 ? 5 : moment(twoAClock).day() >= 1 ? 1 : -2 ),
+    stop:  moment(twoAClock).day( moment(twoAClock).day() >= 5 ? 8 : moment(twoAClock).day() >= 1 ? 5 : 1 )
   },
   {
     // Tuesdays and Friday
     id: 'plastic',
-    start: moment(now).day( moment().day() >= 5 ? 5 : moment().day() >= 2 ? 2 : -2 ),
-    stop:  moment(now).day( moment().day() >= 5 ? 9 : moment().day() >= 2 ? 5 : 2 )
+    start: moment(twoAClock).day( moment(twoAClock).day() >= 5 ? 5 : moment(twoAClock).day() >= 2 ? 2 : -2 ),
+    stop:  moment(twoAClock).day( moment(twoAClock).day() >= 5 ? 9 : moment(twoAClock).day() >= 2 ? 5 : 2 )
   },
   {
     // Wednesday
     id: 'bio',
-    start: moment(now).day( moment().day() >= 3 ? 3 :-4 ),
-    stop:  moment(now).day( moment().day() >= 3 ? 3 :-4 ).add(1,'w')
+    start: moment(twoAClock).day( moment(twoAClock).day() >= 3 ? 3 :-4 ),
+    stop:  moment(twoAClock).day( moment(twoAClock).day() >= 3 ? 3 :-4 ).add(1,'w')
   },
   {
     // Wednesdays
     id: 'glas',
-    start: moment(now).day( moment().day() >= 3 ? 3 :-4 ),
-    stop:  moment(now).day( moment().day() >= 3 ? 3 :-4 ).add(1, 'w')
+    start: moment(twoAClock).day( moment(twoAClock).day() >= 3 ? 3 :-4 ),
+    stop:  moment(twoAClock).day( moment(twoAClock).day() >= 3 ? 3 :-4 ).add(1, 'w')
   },
   {
     // Fridays even weeks
     id: 'metal',
-    start: moment(now).day( moment().isoWeek() % 2 ? -2 : moment().day() >= 5 ? 5 : -9 ).add(moment().isoWeek() % 2, 'w'),
-    stop:  moment(now).day( moment().isoWeek() % 2 ? -2 : moment().day() >= 5 ? 5 : -9 ).add(2, 'w')
+    start: moment(twoAClock).day( moment().isoWeek() % 2 ? -2 : moment(twoAClock).day() >= 5 ? 5 : -9 ).add(moment().isoWeek() % 2, 'w'),
+    stop:  moment(twoAClock).day( moment().isoWeek() % 2 ? -2 : moment(twoAClock).day() >= 5 ? 5 : -9 ).add(2, 'w')
   },
   {
     // Tuesdays and Friday
     id: 'household',
-    start: moment(now).day( moment().day() >= 5 ? 5 : moment().day() >= 2 ? 2 : -2 ),
-    stop:  moment(now).day( moment().day() >= 5 ? 9 : moment().day() >= 2 ? 5 : 2 )
+    start: moment(twoAClock).day( moment(twoAClock).day() >= 5 ? 5 : moment(twoAClock).day() >= 2 ? 2 : -2 ),
+    stop:  moment(twoAClock).day( moment(twoAClock).day() >= 5 ? 9 : moment(twoAClock).day() >= 2 ? 5 : 2 )
   }
 ]
 
@@ -58,6 +58,17 @@ getCurrentPos = function(start, stop, value) {
 
 dates.forEach((bin, i) => {
   let val = getCurrentPos( +bin.start, +bin.stop, +moment() )
-  document.querySelector(`#${bin.id} .bin-fill`).style.width = val * 100 + '%'
-  document.querySelector(`#${bin.id} .answer`).innerHTML = val < 0.3 ? 'ja' : val < 0.75 ? 'kanske' : 'vänta'
+  console.log(bin.id) // eslint-disable-line no-console
+  console.log({val}) // eslint-disable-line no-console
+  if (val < 0) {
+    document.querySelector(`#${bin.id} .bin-fill`).style.width = '100%'
+    document.querySelector(`#${bin.id} .answer`).innerHTML = 'vänta'
+    document.querySelector(`#${bin.id} .next`).innerHTML = 'senare idag'
+    document.querySelector(`#${bin.id} .next`).title = 'Senare idag'
+  } else {
+    document.querySelector(`#${bin.id} .bin-fill`).style.width = val * 100 + '%'
+    document.querySelector(`#${bin.id} .answer`).innerHTML = val < 0.3 ? 'ja' : val < 0.8 ? 'kanske' : 'vänta'
+    document.querySelector(`#${bin.id} .next`).innerHTML = bin.stop.fromNow()
+    document.querySelector(`#${bin.id} .next`).title = bin.stop.format('[På] dddd')
+  }
 });
